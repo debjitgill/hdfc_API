acd3944d23feee666df9406e0c978cfdaf354393
aryanshgill <aryansh.sharma@gillbroking.com>
2024-03-21 14:59:06 +0530

    add modal

--- src/transactionStatus/index.js
+++ src/transactionStatus/index.js
@@ -1,15 +1,16 @@
-import React,{ useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 const TransactionStatus = () => {
+  const [showResponse, setShowResponse] = useState(false);
+  const [response, setResponse] = useState("");
+  const [extractedValue,setExtractedValue] = useState("")
   const [payload, setPayload] = useState({
     payerVA: "",
     amount: "",
   });
   const [msg, setMsg] = useState("");
-  const [valid,setValid] = useState(false)
-  useEffect(() => {
-    
-  }, [payload]);
+  const [valid, setValid] = useState(false);
+  useEffect(() => {}, [payload]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -27,10 +28,19 @@ const TransactionStatus = () => {
 
       if (response.ok) {
         const responseData = await response.json();
+        setResponse(responseData);
+        const dataParts = responseData.data.split("|"); // Corrected line
+        const extractedValue = dataParts[1];
+        setExtractedValue(extractedValue)
+        console.log(extractedValue);
         const token = responseData.token;
         sessionStorage.setItem("token", token);
         setMsg(responseData.msg);
-        setValid(true)
+        setShowResponse(true);
+        setTimeout(() => {
+          setShowResponse(false);
+          setValid(true);
+        }, 4000);
       } else {
         setMsg("Login Failed");
       }
@@ -47,32 +57,65 @@ const TransactionStatus = () => {
     }));
   };
   return (
-    <div className='d-flex align-items-center justify-content-center vh-100'>
-      {!valid && (
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          className="form-control mb-3"
-          name="payerVA"
-          value={payload.payerVA}
-          placeholder="Enter User Virtual Address"
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          className="form-control mb-3"
-          name="amount"
-          value={payload.amount}
-          placeholder="Enter Amount"
-          onChange={handleChange}
-        />
-        <button type="submit" className="btn btn-primary">submit</button>
-        {<p className='text-center'>{msg}</p>}
-      </form>
+    <>
+      <div className="d-flex align-items-center justify-content-center vh-100">
+        <div
+          style={{
+            filter: showResponse ? "blur(5px)" : "none",
+            transition: "filter 0.3s",
+          }}
+        >
+          {!valid && (
+            <form onSubmit={handleSubmit} className="">
+              <input
+                type="text"
+                className="form-control mb-3"
+                name="payerVA"
+                value={payload.payerVA}
+                placeholder="Enter User Virtual Address"
+                onChange={handleChange}
+              />
+              <input
+                type="text"
+                className="form-control mb-3"
+                name="amount"
+                value={payload.amount}
+                placeholder="Enter Amount"
+                onChange={handleChange}
+              />
+              <button type="submit" className="btn btn-primary">
+                submit
+              </button>
+
+              {<p className="text-center">{msg}</p>}
+            </form>
+          )}
+          {valid && <p>Thank You </p>}
+        </div>
+      </div>
+      {showResponse && (
+        <div
+          style={{
+            position: "fixed",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            color: "white",
+            backgroundColor: "black",
+            padding: "2rem",
+            borderRadius: "0.5rem",
+            boxShadow:
+              "0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)",
+          }}
+        >
+          <p style={{ width: "200px", textAlign: "center" }}>
+            {response.message}<br/>
+            {`Your Transaction Reference Id : ${extractedValue}`}
+          </p>
+        </div>
       )}
-      {valid && <p>Thank You </p>}
-    </div>
-  )
+    </>
+  );
 };
 
 export default TransactionStatus;
